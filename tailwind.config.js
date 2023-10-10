abb17bc8f7a1b003705dd1ff9354be09533a2a46
shourya102 <shourya.sahu456@gmail.com>
2023-10-10 17:50:12 +0530

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "public/index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 }
