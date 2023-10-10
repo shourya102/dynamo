7dd2ffdfe0be5d86ddf795ffcde38c926ad67bb2
shourya102 <shourya.sahu456@gmail.com>
2023-10-10 17:23:53 +0530

    init

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
