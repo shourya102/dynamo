54bf93cbccf00c978d3e01b5e848bae8d25fc4a5
shourya102 <shourya.sahu456@gmail.com>
2023-10-12 21:18:52 +0530

    some work on topbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Navbar from "./components/Navbar/Navbar";
-import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home/Home'
 
 function App() {
     return (
         <div className="App">
             <Navbar/>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                </Routes>
+            </Router>
         </div>
     );
 }
