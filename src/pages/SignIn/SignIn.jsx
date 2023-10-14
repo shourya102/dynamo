58a6364a40b1cc151416f870f8cf0e4ac2e77da3
shourya102 <shourya.sahu456@gmail.com>
2023-10-14 15:27:16 +0530

    fixed css in sign in, sign up

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -7,10 +7,10 @@ import './SignIn.css';
 export default function SignIn() {
     return (
         <div className='SignIn flex justify-center items-center'>
-            <div className='fixb flex flex-col w-1/4 shadow-md  bg-skin-base-3 rounded-2xl '>
-                <div className='p-9'>
+            <div className='fixb flex flex-col items-center w-5/6 md:w-3/4 lg:w-1/3 shadow-md  bg-skin-base-3 rounded-2xl'>
+                <div className='p-9 w-full'>
                     <h1 className=' text-title text-center font-semibold my-7'>Sign In</h1>
-                    <form className='flex gap-3 flex-col max-w-lg  ' method='POST' action="">
+                    <form className='flex gap-3 flex-col' method='POST' action="">
                         <input type="email"
                                placeholder='abc123@gmail.com'
                                className='border p-3 rounded-md  focus:outline-none'
@@ -25,8 +25,8 @@ export default function SignIn() {
                                autoComplete='false'
                         />
                         <button
-                            className='bg-slate-700 rounded-md hover:bg-slate-600 focus:text-green-700 p-3 text-white uppercase'>Sign
-                            Up
+                            className='bg-slate-700 rounded-md hover:bg-slate-600 focus:text-green-700 p-3 text-white uppercase'>
+                            Sign In
                         </button>
                     </form>
                     <div className='max-w-lg flex pt-3  gap-3 mx-auto'>
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -2,28 +2,32 @@ import React from 'react'
 import {BiLogoFacebook, BiLogoInstagram, BiLogoLinkedin} from "react-icons/bi";
 import {IconContext} from "react-icons";
 import {Link} from 'react-router-dom';
+import './SignUp.css';
 
 export default function SignUp() {
     return (
-        <div className='max-h-full'>
-            <div className=' flex flex-col max-w-lg mx-auto  shadow-xl my-20 bg-skin-fill-1 rounded-lg '>
-                <div className='p-9'>
+        <div className='SignUp flex justify-center items-center'>
+            <div className='fixb flex flex-col items-center w-5/6 md:w-3/4 lg:w-1/3 shadow-md  bg-skin-base-3 rounded-2xl '>
+                <div className='p-9 w-full'>
                     <h1 className=' text-title  text-center font-semibold my-7'>Sign Up</h1>
-                    <form className='flex gap-3 flex-col max-w-lg  ' method='POST' action="">
+                    <form className='flex gap-3 flex-col w-full' method='POST' action="">
                         <input type="text"
                                placeholder='username'
                                className='border p-3 rounded-md focus:outline-none'
-                               name="" id="username"
+                               name=""
+                               id="username"
                         />
                         <input type="email"
                                placeholder='abc123@gmail.com'
                                className='border p-3 rounded-md focus:outline-none'
-                               name="" id="email"
+                               name=""
+                               id="email"
                         />
                         <input type="password"
-                               placeholder='password.'
+                               placeholder='password'
                                className='border p-3 rounded-md focus:outline-none'
-                               name="" id="password"
+                               name=""
+                               id="password"
                                autoComplete='false'
                         />
                         <button
@@ -32,7 +36,7 @@ export default function SignUp() {
                         </button>
                     </form>
                     <div className='max-w-lg flex pt-3  gap-3 mx-auto'>
-                        <p>Have an account ?</p>
+                        <p>Have an account?</p>
                         <Link className='text-blue-600  hover:text-green-700' to='/sign-in'>
                             <span className=''>Sign in</span>
                         </Link>
