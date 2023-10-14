890579b192fa9ad2835e17e0a956fe82b7b3c774
shourya102 <shourya.sahu456@gmail.com>
2023-10-14 15:09:51 +0530

    fixed indent

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -2,23 +2,26 @@ import React from 'react';
 import {BiLogoFacebook, BiLogoInstagram, BiLogoLinkedin} from "react-icons/bi";
 import {IconContext} from "react-icons";
 import {Link} from 'react-router-dom'
+import './SignIn.css';
 
 export default function SignIn() {
     return (
-        <div className='max-h-full'>
-            <div className=' flex flex-col max-w-lg mx-auto  shadow-xl  bg-skin-fill-1 my-28 rounded-lg '>
+        <div className='SignIn flex justify-center items-center'>
+            <div className='fixb flex flex-col w-1/4 shadow-md  bg-skin-base-3 rounded-2xl '>
                 <div className='p-9'>
                     <h1 className=' text-title text-center font-semibold my-7'>Sign In</h1>
                     <form className='flex gap-3 flex-col max-w-lg  ' method='POST' action="">
                         <input type="email"
                                placeholder='abc123@gmail.com'
                                className='border p-3 rounded-md  focus:outline-none'
-                               name="" id="email"
+                               name=""
+                               id="email"
                         />
                         <input type="password"
-                               placeholder='password.'
+                               placeholder='password'
                                className='border p-3 rounded-md focus:outline-none'
-                               name="" id="password"
+                               name=""
+                               id="password"
                                autoComplete='false'
                         />
                         <button
@@ -27,9 +30,9 @@ export default function SignIn() {
                         </button>
                     </form>
                     <div className='max-w-lg flex pt-3  gap-3 mx-auto'>
-                        <p>Dont't Have an account ?</p>
+                        <p>Don't have an account?</p>
                         <Link className='text-blue-600  hover:text-green-700' to='/sign-up'>
-                            <span className=''>Sign in</span>
+                            <span>Sign Up</span>
                         </Link>
                     </div>
                     <div className='add-top  flex mt-5 p-3 gap-4 justify-center '>
